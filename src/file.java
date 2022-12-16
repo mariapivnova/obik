154aa8c08b22c80aefdfb33950d372d8041a371f
mariapivnova <mariapivnova@yandex.ru>
2022-12-16 14:14:54 +0300

    commit

---
+++ src/file.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class Zipp {
+    public static int  multiply(int a, int b)
+    {
+        if (a == 0 || b == 0)
+            return 0;
+        boolean negative = false;
+        if (a < 0) {
+            negative = true;
+            a = -a;
+        }
+        if (b < 0) {
+            negative = !negative;
+            b = -b;
+        }
+        int result = 0;
+        for (int i = 1; i <= a; ++i)
+            result += b;
+        return negative ? -result : result;
+    }
+    public static void main(final String[] args) {
+        final Scanner scann = new Scanner(System.in);
+        //комментарий
+        System.out.println("Введите первый множитель: ");
+        final int a = scann.nextInt();
+        System.out.println("Введите второй множитель: ");
+        final int b = scann.nextInt();
+        System.out.println("Результат умножения: ");
+        System.out.println(multiply(a, b));
+    }
+}
